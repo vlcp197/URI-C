e1ffe7823b7e1ad639973110958085ac66caa1d6
vlcp197 <vlcp197@gmail.com>
2021-09-19 17:23:01 -0300

    Criei o arquivo URI1020.c

---
+++ URI1020.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    int age, years, months, days,remainder;
+
+    scanf("%i",&age);
+    years = age / 365;
+    remainder = age % 365;
+    months = remainder / 30;
+    remainder = months % 30;
+    days = age - ((months * 30 + years *365));
+
+    printf("%i ano(s)\n",years);
+    printf("%i mes(es)\n",months);
+    printf("%i dia(s)\n",days);
+
+    return 0;
+
+}
